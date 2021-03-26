b2f030db4455937fd555875f7877f0400293ce54
miguel angel espinoza <espinoza.mdin.mig@gmail.com>
2021-03-26 17:22:14 -0600

    Signed-off-by: miguel angel espinoza <espinoza.mdin.mig@gmail.com>

---
+++ E-CommersGapsi/src/main/java/com/mx/gapsi/models/dao/IProveedorDao.java
@@ -0,0 +1,9 @@
+package com.mx.gapsi.models.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.mx.gapsi.models.entity.Proveedor;
+
+public interface IProveedorDao extends CrudRepository<Proveedor, Long>{
+
+}
